3fe86b898d90b6ab43732cf1214a4b1af1e6db01
way2x <kolyazhnetsov@yandex.ru>
2025-10-26 19:14:17 +0300

    add a not working frontend, commit for safety, ill start trying reverse proxy

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,16 @@ import json
 import os
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def root():
     return {"message": "Monitoring Platform API"}
